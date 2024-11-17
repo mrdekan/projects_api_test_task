876b1aa9bc67fb526a936d2f8085e16340da3cf9
mrdekan <yeros05040504@gmail.com>
2024-11-17 16:58:59 +0200

    session time fixed

--- src/auth/module.ts
+++ src/auth/module.ts
@@ -10,7 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '30000s' },
+      signOptions: { expiresIn: '300s' },
     }),
   ],
   providers: [AuthService],
